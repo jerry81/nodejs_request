308f1b2bea450419a25a6a2d65211b5ed7ef51a3
Raskolnikov <jerry.tann@endress.com>
2024-02-28 14:27:44 +0800

    🚧 auth

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 - let's learn node request library and call the netilion api
 
+## steps
+
+- set environment variable AUTHORIZATION_EDGE to base64 encoded u:p, and apikey
+```console
+echo {username}:{password} -n | base64
+export AUTHORIZATION_EDGE={above_result}
+export APIKEY_EDGE={ak...}
+```
+
+## auth with netilion
+
+- apparently all that is needed to sandbox the api is basic auth username:password base64 encoded and a valid api key.
+- set the headers as such and you are good to go.
+```js
+const HEADERS = {
+  'accept': 'application/json',
+  'Authorization': AUTH,
+  'Api-Key': AK
+}
+```
+
 ## proxy
 
 - declare
@@ -21,6 +42,18 @@ request('https://example.com', proxyOptions, function(error, response, body) {
   }
 });
 ```
+
+## capture elapsed time
+
+- before the request goes out capture time
+```js
+  const startTime = Date.now();
+```
+- after the callback, capture the time
+```js
+  const endTime = Date.now();
+```
+
 
 ## TIL
 
--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const request = require('request');
 
 // Define the URLs of the dummy APIs
-const api1Url = 'https://jsonplaceholder.typicode.com/posts/1';
-const api2Url = 'https://jsonplaceholder.typicode.com/posts/2';
+const api2Url = 'https://jsonplaceholder.typicode.com/posts/1';
+const api1Url = 'https://staging-env.api.netilion.endress.com.cn/v1/api_keys';
+const AUTH = `Basic ${process.env.AUTHORIZATION_EDGE}`;
+const AK = process.env.APIKEY_EDGE;
 
 const PROXY_OPTIONS = {
   proxy: 'http://127.0.0.1:4780' // Proxy server address
 }
 
+const HEADERS = {
+  'accept': 'application/json',
+  'Authorization': AUTH,
+  'Api-Key': AK
+}
+
 // Function to make a GET request to an API
-function callApi(url, po = {}) {
+function callApi(url, o = {}) {
     const startT = Date.now();
     return new Promise((resolve, reject) => {
-        request(url,po, (error, response, body) => {
+        request(url,o, (error, response, body) => {
             if (error) {
                 reject(error);
             } else {
                 const endTime = Date.now();
                 const elapsed = endTime - startT;
-                const extraText = JSON.stringify(po)==="{}"?"(no proxy)":"";
+                const extraText = o.proxy?"":"(no proxy)";
                 console.log(`API call to ${url}${extraText} took ${elapsed} milliseconds`);
                 resolve(body);
             }
@@ -29,7 +37,13 @@ function callApi(url, po = {}) {
 // Call the APIs sequentially
 async function main() {
     try {
-        const response1 = await callApi(api1Url, PROXY_OPTIONS);
+        const opt = {
+          // timeout:100,
+          headers: HEADERS,
+          ...PROXY_OPTIONS
+        }
+
+        const response1 = await callApi(api1Url, opt);
         console.log('API 1 Response:', response1);
 
         const response2 = await callApi(api2Url);
